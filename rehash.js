4294655f13fbda046f4bc7798367ef57fea3b636
Gavin Engel <gavin@engel.com>
2016-09-19 17:52:01 -0400

    Update rehash.js

--- rehash.js
+++ rehash.js
@@ -1,10 +1,13 @@
-		// add the URL hash to the HTML-tag, so we can use CSS more effectively
-		function rehash() {
-				var hashAttr = '';
+// add the URL hash to the HTML-tag, so we can use CSS more effectively
+function rehash() {
+	var hashAttr = '';
 
-				if(window.location.hash) {
-				  hashAttr = window.location.hash.replace(/[^a-z0-9]/gi, '');
-				} 
+	if(window.location.hash) {
+	  hashAttr = window.location.hash.replace(/[^a-z0-9]/gi, '');
+	} 
 
-				document.getElementsByTagName('html')[0].setAttribute('hash', hashAttr);
-		}
+	document.getElementsByTagName('html')[0].setAttribute('hash', hashAttr);
+}
+
+rehash();
+window.onhashchange = rehash;
