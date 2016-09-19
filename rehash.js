9264c92f4872cfcf5b1a47b99b3e2b7af0d31284
Gavin Engel <gavin@engel.com>
2016-09-19 17:50:02 -0400

    Create rehash.js

---
+++ rehash.js
@@ -0,0 +1,10 @@
+		// add the URL hash to the HTML-tag, so we can use CSS more effectively
+		function rehash() {
+				var hashAttr = '';
+
+				if(window.location.hash) {
+				  hashAttr = window.location.hash.replace(/[^a-z0-9]/gi, '');
+				} 
+
+				document.getElementsByTagName('html')[0].setAttribute('hash', hashAttr);
+		}
