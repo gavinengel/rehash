147fad80a22d459731731a63f1bc9bbdcd127c25
Gavin Engel <gavin@engel.com>
2017-03-19 14:52:01 -0400

    allows for "/" separators, to create distinct parameters

--- rehash.js
+++ rehash.js
@@ -3,11 +3,26 @@ function rehash() {
   var hashAttr = '';
 
   if(window.location.hash) {
-    hashAttr = window.location.hash.split('?')[0].replace(/[^a-z0-9]/gi, '');
-  } 
+  	// accept only the string before '?' symbol
+    var cleanHash = window.location.hash.split('?')[0].replace(/[^a-z0-9/]/gi, '');
+
+		// split on '/' symbol, to remove excess dividers
+		var pieces = cleanHash.split('/');
+
+		// put the filtered params into clean array
+		var params = [];
+		for (var i in pieces) {
+			if (pieces[i].length) params.push( pieces[i] );
+		}
+
+		// stitch pieces together for the final hashAttr
+		hashAttr = params.join('/');
+  }
 
   document.getElementsByTagName('html')[0].setAttribute('hash', hashAttr);
 }
+
+
 
 rehash(); // initial execution, in case browser opens to page with hash already
 window.addEventListener("hashchange", rehash, false); // execution on subsequent hash updates
